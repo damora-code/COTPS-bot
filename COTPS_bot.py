0decec594a04ef0f175a5f4e7d5fafe9367e6c4f
danny <daniel.morales117@yahoo.com>
2022-04-28 15:01:38 -0400

    Comments for later use

--- COTPS_bot.py
+++ COTPS_bot.py
@@ -1,12 +1,31 @@
 import time
-from selenium import webdriver
+from selenium import webdriver, ActionChains
 
 driver = webdriver.Chrome()
 
 #Test to sure that driver is correctly added
-# driver.get('http://www.google.com')
-# time.sleep(5)
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('ITZhone')
-# search_box.submit()
-# time.sleep(5)
+"""
+driver.get('http://www.google.com')
+time.sleep(5)
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('USDT Price')
+search_box.submit()
+time.sleep(5)
+"""
+
+"""
+Steps
+0. Log in to COTPS (Automate first then come back to this)
+1. Go to Transaction page (refresh?)
+2. Begin transaction
+3. Sell
+4. Confirm
+5. Loop until you can't trade anymore (<$5)
+6. Wait 2 hours to begin next cycle
+"""
+
+driver.get('https://www.cotps.com/#/pages/transaction/transaction')
+create_order = driver.find_element_by_xpath('/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[4]/uni-button')
+create_order.click()
+time.sleep(10)
+sell = driver.find_element_by_xpath
