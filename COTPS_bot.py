c0d32c3cef3a92ff222055cb537b8c3d39c4459e
danny <daniel.morales117@yahoo.com>
2022-04-28 14:30:02 -0400

    first commit

---
+++ COTPS_bot.py
@@ -0,0 +1,12 @@
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+#Test to sure that driver is correctly added
+# driver.get('http://www.google.com')
+# time.sleep(5)
+# search_box = driver.find_element_by_name('q')
+# search_box.send_keys('ITZhone')
+# search_box.submit()
+# time.sleep(5)
