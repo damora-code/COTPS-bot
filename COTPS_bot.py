9b8123e5a543e0d54021595ad5b187d751c7528a
danny <daniel.morales117@yahoo.com>
2022-04-29 12:32:29 -0400

    added sell and confirm

--- COTPS_bot.py
+++ COTPS_bot.py
@@ -14,8 +14,8 @@ Steps
 0. Log in to COTPS --done
 1. Go to Transaction page (refresh?) --done
 2. Begin transaction --done
-3. Sell
-4. Confirm
+3. Sell --done
+4. Confirm --done
 5. Loop until you can't trade anymore (<$5)
 6. Wait 2 hours to begin next cycle
 """
@@ -33,8 +33,31 @@ time.sleep(10)
 driver.get('https://cotps.com/#/pages/transaction/transaction')
 
 #step 2
+time.sleep(5)
+# balance = driver.find_element(by=By.XPATH, value='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[3]/uni-view[2]/uni-view[2]').get_attribute("value")
+# print(balance)
 create_order = driver.find_element(by=By.XPATH, value='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[4]/uni-button').click()
 time.sleep(10)
 
 #step 3 
-#sell = driver.find_element_by_xpath
+sell = driver.find_element(by=By.XPATH, value='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[7]/uni-view/uni-view/uni-view[6]/uni-button[2]').click()
+time.sleep(10)
+
+#step 4
+confirm = driver.find_element(by=By.XPATH, value='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[8]/uni-view/uni-view/uni-button').click()
+time.sleep(5)
+# time.sleep(5)
+# if balance > 5:
+#     create_order = driver.find_element(by=By.XPATH, value='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[4]/uni-button').click()
+#     time.sleep(10)
+
+#     #step 3 
+#     sell = driver.find_element(by=By.XPATH, value='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[7]/uni-view/uni-view/uni-view[6]/uni-button[2]').click()
+#     time.sleep(10)
+
+#     #step 4
+#     confirm = driver.find_element(by=By.XPATH, value='/html/body/uni-app/uni-page/uni-page-wrapper/uni-page-body/uni-view/uni-view[8]/uni-view/uni-view/uni-button').click()
+#     time.sleep(5)
+# else:
+#      time.sleep(7800)
+
