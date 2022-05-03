1dd87cbd35eb55c74cfd6f0ae6c7f4e5cda51a0b
danny <daniel.morales117@yahoo.com>
2022-05-03 11:09:43 -0400

    pep8 installed to format

--- COTPS_bot.py
+++ COTPS_bot.py
@@ -9,19 +9,21 @@ actions = ActionChains(driver)
 number = '3054391830'
 password = 'Guapotrader88!'
 
-#step 0
+# step 0
 driver.get('https://cotps.com/#/pages/login/login?originSource=userCenter')
 time.sleep(5)
-user_element = driver.find_element(by=By.XPATH, value="//uni-view[5]/uni-input/div/input").send_keys(number)
-password_element = driver.find_element(by=By.XPATH, value='//uni-view[7]/uni-input/div/input').send_keys(password)
+user_element = driver.find_element(
+    by=By.XPATH, value="//uni-view[5]/uni-input/div/input").send_keys(number)
+password_element = driver.find_element(
+    by=By.XPATH, value='//uni-view[7]/uni-input/div/input').send_keys(password)
 time.sleep(2)
 login = driver.find_element(by=By.CLASS_NAME, value="login").click()
 
-#step 1
+# step 1
 time.sleep(5)
 driver.get('https://cotps.com/#/pages/transaction/transaction')
 time.sleep(5)
-bal=driver.find_element(By.XPATH, '//uni-view[3]/uni-view[2]/uni-view[2]')
+bal = driver.find_element(By.XPATH, '//uni-view[3]/uni-view[2]/uni-view[2]')
 balance = (float(bal.get_attribute('innerHTML')))
 print(type(balance))
 if balance < 4.4:
@@ -29,7 +31,7 @@ if balance < 4.4:
 else:
     print("greater than 4.4")
 
-#step 2
+# step 2
 # while True:
 #     create_order = driver.find_element(By.CLASS_NAME, 'orderBtn').click()
 #     time.sleep(10)
