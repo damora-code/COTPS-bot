a69bd55f218c6f845f879ff1617b350222efb0d3
danny <daniel.morales117@yahoo.com>
2022-05-03 11:45:34 -0400

    possibly finished loop, test on account with money

--- COTPS_bot.py
+++ COTPS_bot.py
@@ -23,22 +23,23 @@ login = driver.find_element(by=By.CLASS_NAME, value="login").click()
 # step 1
 time.sleep(5)
 driver.get('https://cotps.com/#/pages/transaction/transaction')
-time.sleep(5)
-bal = driver.find_element(By.XPATH, '//uni-view[3]/uni-view[2]/uni-view[2]')
-balance = (float(bal.get_attribute('innerHTML')))
-print(type(balance))
-if balance < 4.4:
-    print("less than 4.4")
-else:
-    print("greater than 4.4")
 
-# step 2
-# while True:
-#     create_order = driver.find_element(By.CLASS_NAME, 'orderBtn').click()
-#     time.sleep(10)
-#     #step 3
-#     sell = driver.find_element(by=By.XPATH, value='//uni-button[2]').click()
-#     time.sleep(10)
-#     #step 4
-#     confirm = driver.find_element(by=By.XPATH, value='//uni-view[8]/uni-view/uni-button').click()
-#     time.sleep(10)
+#step 2
+while True:
+    time.sleep(5)
+    bal = driver.find_element(By.XPATH, '//uni-view[3]/uni-view[2]/uni-view[2]')
+    balance = (float(bal.get_attribute('innerHTML')))
+    print(type(balance))
+    if balance < 5:
+        print("Balance less than 5 please wait")
+        time.sleep(5)
+    else:
+        print("greater than 5")
+        create_order = driver.find_element(By.CLASS_NAME, 'orderBtn').click()
+        time.sleep(10)
+        #step 3
+        sell = driver.find_element(by=By.XPATH, value='//uni-button[2]').click()
+        time.sleep(10)
+        #step 4
+        confirm = driver.find_element(by=By.XPATH, value='//uni-view[8]/uni-view/uni-button').click()
+        time.sleep(10)
