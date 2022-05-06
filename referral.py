fc995a7a54eea3af2cfd2b301d719537fa980f9c
danny <daniel.morales117@yahoo.com>
2022-05-06 12:01:27 -0400

    reads referral balance now

--- referral.py
+++ referral.py
@@ -21,6 +21,7 @@ driver.find_element(by=By.CLASS_NAME, value="login").click()
 time.sleep(2)
 driver.get('https://cotps.com/#/pages/userCenter/myTeam')
 
+time.sleep(5)
 lv1 = driver.find_element(
     by=By.XPATH, value='//uni-view[1]/uni-view[2]/uni-view[2]')
 lv1_bal = (lv1.get_attribute('innerHTML'))
